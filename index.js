2034103bbd1ba6f379926de958cefec5ea459c4a
itsrishti <123953052+itsrishti@users.noreply.github.com>
2023-10-31 21:12:48 +0530

    object

--- index.js
+++ index.js
@@ -1,31 +1,31 @@
-var submitBtn = document.getElementById("submitBtn");
 
-
-submitBtn.test=(e)=>{
+document.getElementById('registration form').addEventListener('submit',function(e)
+{
     e.preventDefault();
-     
-    var fullname= document.getElementById("fullname").value;
-    var username=document.getElementById("username").value;
-    var Address=document.getElementById("Address").value;
-    var DOB=document.getElementById("DOB").value;
-    var Password=document.getElementById("Password").value;
-    var Email=document.getElementById("Email").value;
-   
-    // name+username+Password+DOB+Address+Email);
-    var user = localStorage.setItem("username",username);
-    var pass =localStorage.setItem("Password",Password);
-    var date=localStorage.setItem("DOB",DOB);
-    var add=localStorage.setItem("Address",Address);
-    var name=localStorage.setItem("fullnmae",fullname);
-    var mail=localStorage.setItem("Email",Email);
-    if (fullname==""&&username==""&&Address==""&&DOB==""&&Password==""&&Email==""){
-        "invalid input field";
-    }else{
-        if(fullname=="name"&&username=="user"&&Address==""&&DOB=="date"&&Password=="pass"&&Email=="mail"){
-            "successfully register";
-        }
-        else{
-            "invalid user";
-        }
-    }
-    }
+    const fullname=document.getElementById('fullname').value;
+    const username=document.getElementById('username').value;
+    const Address=document.getElementById('Address').value;
+    const DOB=document.getElementById('DOB').value;
+    // const DOB=document.getElementById('').value;
+    const Email=document.getElementById('Email').value;
+    const Password=document.getElementById('Password').value;
+    
+
+    const formData ={
+        fullname:fullname,
+        usernamre:username,
+        Address:Address,
+        DOB:DOB,
+        Email:Email,
+        Password:Password,
+    };
+    saveFormData(formData);
+});
+function  saveFormData(formData) {
+    const storedFormData =
+    JSON.parse(localStorage.getItem('formData')) || [];
+    storedFormData.push(formData);
+    localStorage.setItem('formData',JSON.stringify(
+        storedFormData));
+
+}
